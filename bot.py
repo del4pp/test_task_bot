371d4035948a53b3ff6951b51f6b77b71d71775f
del4pp <bazalyuk.vitaliy@gmail.com>
2023-09-01 10:08:05 +0300

    Вивід каталогу при натисканні кнопки "каталог товарів"
    Took 12 seconds

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import logging
 from aiogram import Bot, Dispatcher, types, executor
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+import menu
 from config import token
 from model import *
 
@@ -30,6 +32,40 @@ async def start(message: types.Message):
         new_user = Users(chat_id=chat_id, user_name=username, user_first_name=first_name, user_last_name=last_name)
         db.add(new_user)
         db.commit()
+
+    welcome_message = 'Привіт!'
+    await message.answer(welcome_message, reply_markup=menu.home_menu)
+
+
+
+
+#region CallbackMenu
+# Обробник натискання кнопки "Каталог товарів"
+@dp.callback_query_handler(lambda query: query.data == 'open_catalog')
+async def open_catalog(query: types.CallbackQuery):
+    product_keyboard = types.InlineKeyboardMarkup(resize_keyboard=True, selective=True)
+    # Отримання списку товарів з бази даних
+    db = SessionLocal()
+    products = db.query(Product).first()
+
+    #Створення кнопок меню
+    product_keyboard.row(
+        types.InlineKeyboardButton(text='Додати в корзину', callback_data=f'add_basket_{products.id}')
+    )
+    product_keyboard.row(
+        types.InlineKeyboardButton(text='<< Попередній', callback_data=f'prev_{products.id}'),
+        types.InlineKeyboardButton(text='Наступний >>', callback_data=f'next_{products.id}')
+    )
+
+    chat_id = query.from_user.id
+    product_desc = f'*{products.product_name}* \nЦіна: {products.product_price}'
+    with open(products.product_img, 'rb') as product_image:
+        await bot.send_photo(chat_id, photo=product_image, caption=product_desc, parse_mode='Markdown',
+                             reply_markup=product_keyboard)
+
+
+
+#endregion
 
 
 if __name__ == '__main__':
