e252fe5418d8665d6605dc4e8ebb86735833a3c2
del4pp <bazalyuk.vitaliy@gmail.com>
2023-09-01 10:13:25 +0300

    Опрацювання кнопок пагінації
    Took 5 minutes

--- bot.py
+++ bot.py
@@ -64,7 +64,55 @@ async def open_catalog(query: types.CallbackQuery):
                              reply_markup=product_keyboard)
 
 
+@dp.callback_query_handler(lambda query: query.data.startswith(('prev_', 'next_')))
+async def navigate_catalog(query: types.CallbackQuery):
+    action, product_id = query.data.split('_')
+    product_id = int(product_id)
 
+    # Знайдемо товар за product_id
+    db = SessionLocal()
+    products = db.query(Product).all()
+
+    # Знаходимо індекс товару з product_id
+    current_product_index = -1
+    for i, product in enumerate(products):
+        if product.id == product_id:
+            current_product_index = i
+            break
+
+    if current_product_index == -1:
+        # Товар не знайдено, можна обробити цю ситуацію якщо потрібно
+        return
+
+    if query.data.startswith('prev_'):
+        current_product_index -= 1
+        if current_product_index < 0:
+            current_product_index = len(products) - 1
+    elif query.data.startswith('next_'):
+        current_product_index += 1
+        if current_product_index >= len(products):
+            current_product_index = 0
+
+    current_product = products[current_product_index]
+
+    product_keyboard = types.InlineKeyboardMarkup(resize_keyboard=True, selective=True)
+    product_keyboard.row(
+        types.InlineKeyboardButton(text='Додати в корзину', callback_data=f'add_basket_{current_product.id}')
+    )
+    product_keyboard.row(
+        types.InlineKeyboardButton(text='<< Попередній', callback_data=f'prev_{current_product.id}'),
+        types.InlineKeyboardButton(text='Наступний >>', callback_data=f'next_{current_product.id}')
+    )
+
+    chat_id = query.from_user.id
+    # Формуємо новий текст і кнопки для повідомлення
+    product_desc = f'*{current_product.product_name}* \nЦіна: {current_product.product_price}'
+    with open(current_product.product_img, 'rb') as product_image:
+        # Використовуємо метод edit_message_text для оновлення повідомлення
+        await bot.edit_message_media(chat_id=chat_id, message_id=query.message.message_id,
+                                     media=types.InputMediaPhoto(media=product_image), reply_markup=product_keyboard)
+        await bot.edit_message_caption(chat_id=chat_id, message_id=query.message.message_id, caption=product_desc,
+                                       parse_mode='Markdown', reply_markup=product_keyboard)
 #endregion
 
 
